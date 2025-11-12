4a4e704a74739c9dc0d5d871af33780becf1e2e7
Quoc Dat Ngo <z482360@ad.unsw.edu.au>
2025-11-12 14:43:00 +1100

    Added CommandLineRunner for the application to test the new Run record and log it out to the console

--- src/main/java/dev/kevinngo/runnerz/RunnerzApplication.java
+++ src/main/java/dev/kevinngo/runnerz/RunnerzApplication.java
@@ -1,14 +1,30 @@
 package dev.kevinngo.runnerz;
 
+import dev.kevinngo.runnerz.run.Location;
+import dev.kevinngo.runnerz.run.Run;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class RunnerzApplication {
+	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
-		System.out.println("Init main class");
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+			log.info("Run: " + run);
+		};
+	}
 }
