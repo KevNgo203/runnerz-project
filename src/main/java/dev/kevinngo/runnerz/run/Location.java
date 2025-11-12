e94e13452e165a33e83f632f99d11bbc597000e9
Quoc Dat Ngo <z482360@ad.unsw.edu.au>
2025-11-12 14:41:49 +1100

    Create new Run record that works as an instance of an actual running session

---
+++ src/main/java/dev/kevinngo/runnerz/run/Location.java
@@ -0,0 +1,5 @@
+package dev.kevinngo.runnerz.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
---
+++ src/main/java/dev/kevinngo/runnerz/run/Run.java
@@ -0,0 +1,14 @@
+package dev.kevinngo.runnerz.run;
+
+import java.time.LocalDateTime;
+
+public record Run(
+        Integer id,
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
+        Integer kilometers,
+        Location location
+) {
+
+}
