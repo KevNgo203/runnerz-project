d1d01a30e7cb99574070d5bc46133ad9d7e9c221
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2025-11-21 22:30:15 +1100

    Construct API endpoints using Spring MVC and implement corresponding methods

---
+++ src/main/java/dev/kevinngo/runnerz/run/RunRepository.java
@@ -0,0 +1,55 @@
+package dev.kevinngo.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+    private final List<Run> runs = new ArrayList<Run>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id().equals(id))
+                .findFirst();
+    }
+
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        existingRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(run -> run.id().equals(id));
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR));
+
+        runs.add(new Run(2,
+                "Wednesday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+                6,
+                Location.INDOOR));
+    }
+}
