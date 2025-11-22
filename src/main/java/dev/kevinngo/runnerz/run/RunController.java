f18557f81207b17d8b00eec114725f0ea7d386b7
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2025-11-22 18:31:15 +1100

    Add API input validation

--- src/main/java/dev/kevinngo/runnerz/run/RunController.java
+++ src/main/java/dev/kevinngo/runnerz/run/RunController.java
@@ -1,5 +1,6 @@
 package dev.kevinngo.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -26,7 +27,7 @@ public class RunController {
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new RunNotFoundException();
         }
         return run.get();
     }
@@ -34,14 +35,14 @@ public class RunController {
     // Post
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run) {
+    void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
     // Put
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
