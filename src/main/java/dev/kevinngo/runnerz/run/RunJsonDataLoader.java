8319aa9a2df15d8b16902e68b74febfdb5f83547
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2025-11-25 11:44:33 +1100

    Fix the field name of the JSON data file to match the Run record

--- src/main/java/dev/kevinngo/runnerz/run/RunJsonDataLoader.java
+++ src/main/java/dev/kevinngo/runnerz/run/RunJsonDataLoader.java
@@ -1,31 +1,37 @@
 package dev.kevinngo.runnerz.run;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Component
-public class RunJsonDataLoader implements CommandLineRunner{
-    private static final Logger log = LoggerFactory.getLogger(RunJsonDataLoader.class);
-    private final RunRepository runRepository;
-    private final ObjectMapper objectMapper;
+public class RunJsonDataLoader implements CommandLineRunner {
 
-    public RunJsonDataLoader(RunRepository runRepository, ObjectMapper objectMapper) {
-        this.runRepository = runRepository;
+    private static final Logger log = LoggerFactory.getLogger(RunJsonDataLoader.class);
+    private final ObjectMapper objectMapper;
+    private final RunRepository runRepository;
+
+    // Inject the JSON file from classpath
+    @Value("classpath:data/runs.json")
+    private Resource runsJson;
+
+    public RunJsonDataLoader(ObjectMapper objectMapper, RunRepository runRepository) {
         this.objectMapper = objectMapper;
+        this.runRepository = runRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        if (runRepository.count() == 0) {
-            try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/runs.json")) {
+        if(runRepository.count() == 0) {
+            try (var inputStream = runsJson.getInputStream()) {
                 Runs allRuns = objectMapper.readValue(inputStream, Runs.class);
                 log.info("Reading {} runs from JSON data and saving to in-memory collection.", allRuns.runs().size());
                 runRepository.saveAll(allRuns.runs());
@@ -33,7 +39,8 @@ public class RunJsonDataLoader implements CommandLineRunner{
                 throw new RuntimeException("Failed to read JSON data", e);
             }
         } else {
-            log.info("Not loading Runs from JSON data because the collection already contains data.");
+            log.info("Not loading Runs from JSON data because the collection contains data.");
         }
     }
+
 }
