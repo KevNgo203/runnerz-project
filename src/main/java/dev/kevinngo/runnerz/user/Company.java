74872aadb2f453c2c9a47a4b9970353c527f00f5
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2025-11-26 11:53:47 +1100

    Create new object based on users' data; Create UserRestClient and UserHttpClient to make request to other services

---
+++ src/main/java/dev/kevinngo/runnerz/user/Company.java
@@ -0,0 +1,15 @@
+package dev.kevinngo.runnerz.user;
+
+/*
+"company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+}
+ */
+public record Company(
+        String name,
+        String catchPhrase,
+        String bs
+) {
+}
