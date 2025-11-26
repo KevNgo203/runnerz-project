4abb3003888dc703e01db414dc474fa4b376ab45
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2025-11-26 11:54:47 +1100

    Create instance of UserHttpClient then make request, i.e: executing a method

--- src/main/java/dev/kevinngo/runnerz/RunnerzApplication.java
+++ src/main/java/dev/kevinngo/runnerz/RunnerzApplication.java
@@ -1,9 +1,19 @@
 package dev.kevinngo.runnerz;
 
+import dev.kevinngo.runnerz.user.User;
+import dev.kevinngo.runnerz.user.UserHttpClient;
+import dev.kevinngo.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @SpringBootApplication
 public class RunnerzApplication {
@@ -13,11 +23,18 @@ public class RunnerzApplication {
 		SpringApplication.run(RunnerzApplication.class, args);
 	}
 
-	// @Bean
-	// CommandLineRunner runner(RunRepository runRepository) {
-	// 	return args -> {
-	// 		Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
-	// 		runRepository.create(run);
-	// 	};
-	// }
+	@Bean
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	 @Bean
+	 CommandLineRunner runner(UserRestClient client) {
+	 	return args -> {
+			 List<User> users = client.findAll();
+			System.out.println(users);
+		};
+	 }
 }
